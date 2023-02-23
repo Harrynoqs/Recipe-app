d9f5638886a63e081b0b870959d84fa4b532b6db
Degisew <jegisew21@gmail.com>
2023-02-23 13:43:02 +0300

    implemented foods action functionality

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,71 +1,48 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[show edit update destroy]
-
   # GET /foods or /foods.json
   def index
-    @foods = Food.all
+    @foods = Food.all.where(user_id: current_user.id).order(created_at: :desc)
   end
 
   # GET /foods/1 or /foods/1.json
-  # def show
-  # end
+  def show
+    @food = Food.find_by(id: params[:id])
+  end
 
   # GET /foods/new
   def new
     @food = Food.new
   end
 
-  # GET /foods/1/edit
-  # def edit
-  # end
-
   # POST /foods or /foods.json
   def create
-    @food = Food.new(food_params)
+    # @food = Food.new(food_params)
+    # @food = current_user.foods.new(food_params)
+    measurement_unit = params[:food][:measurement_unit]
+    prices = params[:food][:price]
+    quantity = params[:food][:quantity]
+
+    @food = Food.new(user: current_user, name: params[:food][:name], measurement_unit:, price: prices, quantity:)
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
-        format.json { render :show, status: :created, location: @food }
+        format.html { redirect_to foods_path, notice: 'Food was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /foods/1 or /foods/1.json
-  def update
-    respond_to do |format|
-      if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
-        format.json { render :show, status: :ok, location: @food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /foods/1 or /foods/1.json
   def destroy
-    @food.destroy
+    @food = Food.find_by(id: params[:id])
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
-      format.json { head :no_content }
+      if @food.destroy
+        format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      else
+        format.html { redirect_to foods_url, error: 'Failed to Delete the Food.' }
+      end
     end
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_food
-    @food = Food.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def food_params
-    params.require(:food).permit(:name, :measurement - unit, :price, :quantity)
   end
 end
