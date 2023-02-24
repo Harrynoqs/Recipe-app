6cf35c8d25c1f283bcee7d0df08f7e1963b2d67c
Degisew <jegisew21@gmail.com>
2023-02-25 01:23:50 +0300

    added public recepies page

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,18 +1,16 @@
 class ShoppingListsController < ApplicationController
   def index
-    @recipe = Recipe.find(params[:recipe_id])
-    @shopping_lists = @recipe.recipe_foods.includes([:food])
-    @total = sum(@shopping_lists)
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    @shopping_lists = RecipeFood.includes(:food).all
+    @total_price = sum(@shopping_lists)
   end
 
   private
 
   def sum(shopping_lists)
     total = 0
-    total_items = 0
     shopping_lists.each do |item|
       total += item.food.price * item.quantity.to_i
-      total_items+=1
     end
     total
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   resources :recipes do
   resources :recipe_foods
   end
+  resources :shopping_lists, only: [:index]
   resources :foods
+  resources :public_recipes, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "public_recipes#index"
 end
